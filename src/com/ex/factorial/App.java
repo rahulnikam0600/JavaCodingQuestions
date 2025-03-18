958951d62efdf1d3a93c56e8533d3090a0cf6cca
rahulnikam0600 <rahulnikam0600@gmail.com>
2025-03-18 13:48:54 +0530

    factorial of number code

---
+++ src/com/ex/factorial/App.java
@@ -0,0 +1,12 @@
+package com.ex.factorial;
+
+public class App {
+	public static void main(String[] args) {
+		int n = 7;
+		int result = 1;
+		for(int i = 1; i <= n; i++) {
+			result *= i;
+		}
+		System.out.println(result);
+	}
+}
